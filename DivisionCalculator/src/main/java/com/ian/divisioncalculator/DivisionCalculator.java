5481be5505e26d73ad9ca22fbc72c2718d580bbc
iankristoper <iankristoper26@gmail.com>
2024-12-03 04:07:30 +0800

    initial commit

--- DivisionCalculator/src/main/java/com/ian/divisioncalculator/DivisionCalculator.java
+++ DivisionCalculator/src/main/java/com/ian/divisioncalculator/DivisionCalculator.java
@@ -1,16 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ian.divisioncalculator;
 
-/**
- *
- * @author Ian Tagano
- */
+
+/*
+    Description:
+    Create a program that takes two numbers as input and performs division. 
+    Use exception handling to manage scenarios like division by zero or invalid input.
+*/
+
+
+import java.util.Scanner;
+
+
+
 public class DivisionCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        try{
+            
+            System.out.print("Enter a numerator: ");
+            String numerator = scanner.nextLine();
+            
+            System.out.print("Enter a denominator: ");
+            String denominator = scanner.nextLine();
+            
+            //convert string to integers
+            int num1 = Integer.parseInt(numerator);
+            int num2 = Integer.parseInt(denominator);
+            
+            int result = num1 / num2;
+            
+            System.out.println("Result: " + result); 
+            
+        } catch(ArithmeticException e) {
+            
+            System.out.println("Error cannot divide by zero");
+                    
+        } catch(NumberFormatException e) {
+            
+            System.out.println("Error invalid input");
+            
+        } finally {
+            
+            //this block executes no matter what happens
+            System.out.println("Thank you for using my simple program");
+            
+        }
     }
 }
