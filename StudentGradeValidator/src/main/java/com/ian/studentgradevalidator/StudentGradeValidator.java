690312b19cf8bd8cc2993838ab2a113e4c734eeb
iankristoper <iankristoper26@gmail.com>
2024-12-05 03:30:52 +0800

    initial commit

--- StudentGradeValidator/src/main/java/com/ian/studentgradevalidator/StudentGradeValidator.java
+++ StudentGradeValidator/src/main/java/com/ian/studentgradevalidator/StudentGradeValidator.java
@@ -42,8 +42,65 @@ public class StudentGradeValidator {
         Map<String, Double> studentGrades = new HashMap<>();
         
         System.out.println("Student Grade Validator");
-        System.out.println("Enter student name and grade. Type 'done' to finish.\n");
         
+        while(true) {
+            
+            try {
+                
+                String terminator;
+                
+                //input student name 
+                System.out.print("Enter student name: ");
+                String name = scanner.nextLine();
+                
+                //validate the input
+                if(name.isBlank()) {
+                    throw new EmptyNamesException("Student name should not be blank!");
+                }
+                
+                
+                //input student grade 
+                System.out.print("Enter grade for " + name + ": ");
+                double grade = scanner.nextDouble();
+                scanner.nextLine();
+                
+                //validate the input
+                if(grade < 0.0 || grade > 100.0) {
+                    throw new InvalidGradeException("Number should not below 0 and above 100");
+                }
+                               
+                //store the valid data 
+                studentGrades.put(name, grade);
+                
+            } catch(NumberFormatException e) {
+                
+                System.out.println("Error: Grade must be numeric value");
+                
+            } catch(InvalidGradeException e) {
+                
+                System.out.println("Error: " + e.getMessage());
+                
+            } catch(EmptyNamesException e) {
+                
+                System.out.println("Error: " + e.getMessage());
+                
+            }
+            
+            System.out.print("Enter \"done\" if you are done: " );
+            String terminator = scanner.nextLine();
+            
+            if(terminator.equalsIgnoreCase("done")) {
+                break;
+            }
+        }
+        
+        // Display valid entries
+        System.out.println("\nValid Student Grades:");
+        for (Map.Entry<String, Double> entry : studentGrades.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        scanner.close();
         
     }
 }
